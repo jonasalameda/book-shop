0f59e2586899e1fd2d5ce8ed8dfeeeb67fd36430
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-11-03 21:29:35 -0500

    chore: styling on main page

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?></title>
+    <link rel="stylesheet" href="/book-shop/public/assets/css/navbar.css">
     <!-- TODO: include your CSS files here -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
+<nav class="navbar" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/book-shop/">
+      <img src="/book-shop/public/assets/imgs/tail.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Ruina
+    </a>
+    <a class="nav-link" href="book-shop/featured">Featured</a>
+    <a class="nav-link" href="book-shop/catalog">Catalog</a>
+    <a class="nav-link" href="book-shop/contact">Contact Us</a>
+  </div>
+</nav>
--- app/Views/homeView.php
+++ app/Views/homeView.php
@@ -6,15 +6,22 @@ $page_title = 'Home';
 ViewHelper::loadHeader($page_title);
 ?>
 
-<h1>Slim Framework-based MVC Application</h1>
-<p>This is a simple MVC application built with Slim Framework.</p>
+<div class="container-fluid d-flex flex-column justify-content-center mb-3">
+    <div class="">
+        <h1>Test Title</h1>
+    </div>
 
-<p>This app uses a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.</p>
+    <div class="col">
+        <p>This is a simple MVC application built with Slim Framework.</p>
 
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+        <p>This app uses a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.</p>
 
-<a href="./shops">Shops</a>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+    </div>
+</div>
+
+
 
 <?php
 
