4a948dd43739f6bfbc5d530f3f31d23d0994f6b4
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-11-19 09:07:53 -0500

    chore: update footer and navbar

--- app/Views/common/footer.php
+++ app/Views/common/footer.php
@@ -1,3 +1,21 @@
+<div id="footer">
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col">
+                <a class="footer-link" href="#">Conditions of use</a>
+                <a class="footer-link" href="#">Privacy Notice</a>
+                <a class="footer-link" href="#">Cookies Notice</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row text-center">
+            <p>&copy; 2025, Jonas and Nicolas. All rights reserved.</p>
+        </div>
+    </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -5,22 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?></title>
-    <link rel="stylesheet" href="/book-shop/public/assets/css/navbar.css">
     <link rel="stylesheet" href="/book-shop/public/assets/css/index.css">
+    <link rel="stylesheet" href="/book-shop/public/assets/css/footer.css">
     <!-- TODO: include your CSS files here -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/book-shop/public/assets/css/navbar.css">
 </head>
 
 <body>
-
-<nav class="navbar" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/book-shop/">
-      <img src="/book-shop/public/assets/imgs/tail.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      Ruina
-    </a>
-    <a class="nav-link" href="/book-shop/featured">Featured</a>
-    <a class="nav-link" href="/book-shop/catalog">Catalog</a>
-    <a class="nav-link" href="/book-shop/contact">Contact Us</a>
-  </div>
-</nav>
+    <nav class="navbar navbar-main" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/book-shop/">
+                <img src="/book-shop/public/assets/imgs/tail.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Ruina
+            </a>
+            <a class="nav-link" href="/book-shop/featured">Featured</a>
+            <a class="nav-link" href="/book-shop/catalog">Catalog</a>
+            <a class="nav-link" href="/book-shop/contact">Contact Us</a>
+        </div>
+    </nav>
